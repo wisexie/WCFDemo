06baf79711cbd1c3841d0cb47f389f4261e97a62
wise <864792642@qq.com>
2015-10-14 17:30:01 +0800

    添加

---
+++ LoadBalanceClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using Com.FrameWork.Helper;
+using Com.FrameWork.Helper.Wcf.LoadBalance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadBalanceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BalanceServer.BalanceStart<IHeatBeat>();
+        }
+    }
+}
